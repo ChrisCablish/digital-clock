21641b9fc53cf67cdfbcc060ecd58d8d9e9eabf7
Chris <chriscablish@hotmail.com>
2022-12-30 09:53:42 -0600

    debug and complete javascript

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const minuteEl = document.getElementById("minutes");
 const secondEl = document.getElementById("seconds")
 const ampmEl = document.getElementById("ampm");
 
+
 function updateClock() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
@@ -13,6 +14,12 @@ function updateClock() {
         h = h -12
         ampm = "PM";
     }
+   
+    /*ternary operator - if h is less than ten, h is equal to 0 + h*/
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
    hourEl.innerText = h;
    minuteEl.innerText = m;
